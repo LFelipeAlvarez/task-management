3d4bc54667af5a4d775746eebbeff7bc48e33c16
unknown <lufealsu@hotmail.com>
2024-05-14 18:19:52 -0500

    setting enviroment vars for deploy

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,4 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-
-  // <React.StrictMode>
-    // <BrowserRouter>
-      <App />
-    // </BrowserRouter>
-  // </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
